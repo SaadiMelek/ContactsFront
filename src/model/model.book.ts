adad126ff49f0a9c5a693a4361b0cb8f65a5d274
SaadiMelek <saadi.melek@gmail.com>
2019-03-19 12:07:13 +0100

    first commit

---
+++ src/model/model.book.ts
@@ -0,0 +1,14 @@
+export class Contact {
+  id : any=null;
+  nom : string="";
+  prenom : string="";
+  email : string="";
+  dateNaissance : Date;
+  tel:string="";
+  photo:string="";
+
+  /*constructor(public nom:string, public prenom:string, public email:string, public dateNaissance:Date, public telephone:string, public photo:string){
+
+  }*/
+  
+}
